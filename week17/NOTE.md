4e0f3e361dbb4c97754bdb47d1b94e699bfd8981
fanwalker <fwj916889672@gmail.com>
2020-08-05 23:21:43 +0800

    feat 工具链、整体理解一个工具链的设计

--- week17/NOTE.md
+++ week17/NOTE.md
@@ -1,1 +1,23 @@
-# 每周总结可以写在这里
+### 工具
+#### 初始化
+- yeoman
+- create-react-app
+- vue-cli
+
+#### 开发/调试
+- dev-tool/chrome
+- webpack-dev-server
+- mock
+- wireshark
+- charles
+
+#### 测试
+- mocha
+- jest
+
+#### 发布
+- lint
+- jenkins
+
+为啥 yo + 起的名字能执行？
+npm link 将本地开发的generator，链接到全局npm包中（相当于全局安装了generator-walker这个包，当调用yo walker的时候，yeoman会找名字为generator-walker这个包，并且执行
---
+++ week17/toolchain/yeoman/generator-walker/generators/app/index.js
@@ -0,0 +1,30 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // This makes `appname` a required argument.
+    // this.argument("appname", { type: String, required: true });
+
+    // And you can then access it later; e.g.
+    // this.log(this.options.appname);
+  }
+
+  async prompting() {
+    this.answers = await this.prompt([{
+      type    : 'input',
+      name    : 'title',
+      message : 'Your project title',
+    }]);
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      { title: this.answers.title } // user answer `title` used
+    );
+  }
+};
